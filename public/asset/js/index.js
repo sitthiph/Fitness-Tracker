18d407d5e357b5efb6971b48ee462d7d95040712
Sitthiph <47881002+sitthiph@users.noreply.github.com>
2019-11-05 18:45:16 -0500

    Added add field

---
+++ public/asset/js/index.js
@@ -0,0 +1,7 @@
+// wait for HTML to finish loading before loading any DOM;
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("submit");
+    button.addEventListener("click", function () {
+        if()
+    });
+});
